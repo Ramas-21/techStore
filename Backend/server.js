00ca0103429820cafaababb4af13632420b93cac
lawrence <lawrencehome21@gmail.com>
2024-08-12 23:31:41 +0300

    included the port value in the env file (better practice)

--- Backend/server.js
+++ Backend/server.js
@@ -4,11 +4,12 @@ import { connectDB } from "./config/db.js";
 import productRoutes from './routes/product.route.js'
 dotenv.config();
 const app = express();
+const PORT = process.env.PORT || 5000
 app.use(express.json()); // allows us to accept JSON data in the req.body
 
 app.use("/api/products", productRoutes);
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   connectDB();
   console.log("Server has started");
 });
